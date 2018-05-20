e7978b92b1358bfb17b61536bcf601192f85e083
Salome Lambermont <salome.lambermont@gmail.com>
2018-05-20 17:56:04 +0100

    Add game object, rename frame object

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,51 +1,13 @@
-describe('Frame', function() {
-  var frame
+describe('Game', function() {
+  var game
 
-  beforeEach(function () {
-    frame = new Frame();
+  beforeEach(function(){
+    game = new Game;
   });
 
-  describe('Frame', function() {
-    it('should default roll 1 and 2 to nothing', function() {
-      expect(frame.roll1).toEqual('-')
-      expect(frame.roll2).toEqual('-')
-    });
-    it('should default to not completed until roll2 happens', function() {
-      expect(frame.completed).toEqual(false)
-    });
-  });
-
-  describe('#firstRoll', function() {
-    it('should count number of pins down in Frame object', function() {
-      frame.firstRoll(4);
-      expect(frame.roll1).toEqual(4);
-    });
-    it('should end the frame if strike', function() {
-      frame.firstRoll(10);
-      expect(frame.roll2).toEqual(0);
-      expect(frame.firstRoll(10)).toEqual('strike!')
-    });
-    it('should throw error if knocking more pins than there are', function() {
-      expect(function() {
-        frame.firstRoll(11)
-      }).toThrow('Not enough pins left');
-    });
-  });
-
-  describe('#secondRoll', function() {
-    it('should count number of pins down in Frame object', function() {
-      frame.secondRoll(4);
-      expect(frame.roll2).toEqual(4);
-    });
-    it('should throw error if knocking more pins than there are', function() {
-      expect(function() {
-        frame.firstRoll(4);
-        frame.secondRoll(7);
-      }).toThrow('Not enough pins left');
-    });
-    it('should give a spare message if rolling a spare', function() {
-      frame.firstRoll(4);
-      expect(frame.secondRoll(6)).toEqual('spare!');
+  describe('#scores', function() {
+    it('should default to an empty array', function(){
+      expect(game.scores).toEqual([]);
     });
   });
 });
--- src/game.js
+++ src/game.js
@@ -1,26 +1,3 @@
-function Frame() {
-  this.roll1 = '-';
-  this.roll2 = '-';
-  this.completed = false
-}
-
-Frame.prototype.firstRoll = function(pinsDown) {
-  if(pinsDown > 10) {
-    throw 'Not enough pins left'
-  } else if(pinsDown === 10) {
-    this.roll2 = 0;
-    this.completed = true;
-    return 'strike!'
-  }
-  this.roll1 = pinsDown;
-}
-
-Frame.prototype.secondRoll = function(pinsDown) {
-  if(pinsDown > 10-this.roll1) {
-    throw 'Not enough pins left'
-  } else if(pinsDown === 10-this.roll1) {
-    return 'spare!'
-  }
-  this.roll2 = pinsDown
-  this.completed = true
+function Game() {
+  this.scores = []
 }
